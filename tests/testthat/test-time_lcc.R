039796ddb084082ba835802ff1f366906675e8a0
Prof-ThiagoOliveira <thiago.paula.oliveira@usp.br>
2018-11-07 17:13:11 -0200

    time_lcc

--- tests/testthat/test-time_lcc.R
+++ tests/testthat/test-time_lcc.R
@@ -3,5 +3,5 @@ context("running regular sequence")
 test_that("runing regular sequence with experimental time from 0 to 10",{
   time<-seq(0,10)
   expected<-unique(sort(c(time,seq(0,10,length.out = 30))))
-  expect_equal(expected, time_lcc(time = time, from = 0, to = 1, n = 30))
+  expect_equal(expected, time_lcc(time = time, from = 0, to = 10, n = 30))
 })
