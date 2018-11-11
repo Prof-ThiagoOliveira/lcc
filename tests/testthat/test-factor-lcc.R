f009b85c7f5bc2b3a9169a32e9f70ea0e0730f39
Prof-ThiagoOliveira <thiago.paula.oliveira@usp.br>
2018-11-11 18:56:51 -0200

    lcc-time-scale-test

--- tests/testthat/test-factor-lcc.R
+++ tests/testthat/test-factor-lcc.R
@@ -14,18 +14,3 @@ expect_that(fm2<-lcc(dataset = hue2, subject = "Fruit", resp = "H_mean", method 
 expect_equivalent(fm1$Summary.lcc,fm2$Summary.lcc)
 })
 
-
-test_that("change in the time scale does not affect lcc estimates", {
-  data(hue)
-  hue$Time2<-hue$Time+10
-  hue$Time3<-scale(hue$Time)
-  expect_that(fm1<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
-  
-  expect_that(fm2<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time2", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
-  
-  expect_that(fm3<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time3", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
-  
-  expect_equal(fm1$Summary.lcc$fitted[,-1],fm2$Summary.lcc$fitted[,-1],tolerance = 1e-4)
-  expect_equal(fm1$Summary.lcc$fitted[,-1],fm3$Summary.lcc$fitted[,-1],tolerance = 1e-4)
-})
-
---
+++ tests/testthat/test-time-scale-lcc.R
@@ -0,0 +1,19 @@
+library("testthat")
+library("lcc")
+
+context("scale handling in time variable")
+
+test_that("changes in the time scale does not affect lcc estimates", {
+  data(hue)
+  hue$Time2<-hue$Time+10
+  hue$Time3<-scale(hue$Time)
+  expect_that(fm1<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
+  
+  expect_that(fm2<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time2", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
+  
+  expect_that(fm3<-lcc(dataset = hue, subject = "Fruit", resp = "H_mean", method = "Method", time = "Time3", qf = 2, qr = 2, components = TRUE), is_a("lcc"))
+  
+  expect_equal(fm1$Summary.lcc$fitted[,-1],fm2$Summary.lcc$fitted[,-1],tolerance = 1e-4)
+  expect_equal(fm1$Summary.lcc$fitted[,-1],fm3$Summary.lcc$fitted[,-1],tolerance = 1e-4)
+})
+
