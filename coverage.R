8b8a00b8863a99e066bcadb2c6c0d85ea6bf4c2e
Prof-ThiagoOliveira <thiago.paula.oliveira@usp.br>
2019-11-03 15:32:21 +0000

    travis.update

---
+++ coverage.R
@@ -0,0 +1,19 @@
+
+## Script to run the coverage tests.  This is needed for running the tests on
+## travis CI because the travis_wait command mangles the arguments you need to
+## put the commands inline in the after_success section
+
+library(covr)
+Sys.setenv("lcc.is_coverage_test" = "TRUE")
+x <- package_coverage()
+cat("coverage = ", percent_coverage(x), " %\n")
+codecov(coverage = x)
+## Script to run the coverage tests.  This is needed for running the tests on
+## travis CI because the travis_wait command mangles the arguments you need to
+## put the commands inline in the after_success section
+
+library(covr)
+Sys.setenv("lcc.is_coverage_test" = "TRUE")
+x <- package_coverage()
+cat("coverage = ", percent_coverage(x), " %\n")
+codecov(coverage = x)
