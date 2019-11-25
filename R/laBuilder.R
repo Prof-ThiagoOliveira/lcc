483188532ecd941b5f16ce4350926a2ac84e7635
Prof-ThiagoOliveira <thiago.paula.oliveira@usp.br>
2019-11-25 22:16:04 +0000

    lcc-version-1.0.3

--- R/laBuilder.R
+++ R/laBuilder.R
@@ -33,60 +33,63 @@ laBuilder <- function(G, diffbeta, tk, q_r, q_f, g, sig2_epsilon,
   Tk_f <- sapply(0:q_f, function(x) tk^x)
   S2 <- try((as.matrix(Tk_f[,c(1:(length(diffbeta)))]) %*% (diffbeta))^2,
             silent = TRUE)
-  if(class(S2) == "try-error") {
-    stop(print("Please, change the name of factor levels associated with method argument. Example:  levels M1, M2, M3, ..., Mn, with n=0, 1, 2, ..., N."), call. = FALSE)
-  }
-  tGt <- diag(Tk_r %*% G %*% t(Tk_r))
-  varcomp <- summary(model)
-  var.f <- class(varcomp$modelStruct$varStruct)[1]
-  LA <- list()
-  #---------------------------------------------------------------------
-  # If the var.class = varIdent
-  #---------------------------------------------------------------------
-  if(var.f == "varIdent") {
-    gd <- g(delta)
-    gdl <- g(deltal)
-    ldb2 <- length(gdl)
-    v <- list()
-    u <- list()
-    for(i in 1:ldb2){
-      v[[i]]<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl[i]))
-      u[[i]]<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl[i]))^(1/4)
-      LA[[i]] <- as.numeric(2 / (v[[i]]+v[[i]]^(-1)+u[[i]]^2))
-    }
-  }
-  #---------------------------------------------------------------------
-  # If the var.class = varExp
-  #---------------------------------------------------------------------
-  if(var.f == "varExp") {
-    if(attr(varcomp$modelStruct$varStruct, "formula")==~time){
-      gd <- g(delta,tk)
-      gdl <- g(deltal,tk)
-      v<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl))
-      u<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl))^(1/4)
-      LA <- list(2 / (v+v^(-1)+u^2), NA)
-    } else {if(attr(varcomp$modelStruct$varStruct, "formula")==~time | method){
-      gd <- g(delta,tk)
-      ldb2 <- length(deltal)
-      gdl<-list()
+  if(all(class(S2) != "try-error")) {
+    tGt <- diag(Tk_r %*% G %*% t(Tk_r))
+    varcomp <- summary(model)
+    var.f <- class(varcomp$modelStruct$varStruct)[1]
+    LA <- list()
+    #---------------------------------------------------------------------
+    # If the var.class = varIdent
+    #---------------------------------------------------------------------
+    if(var.f == "varIdent") {
+      gd <- g(delta)
+      gdl <- g(deltal)
+      ldb2 <- length(gdl)
       v <- list()
       u <- list()
       for(i in 1:ldb2){
-        gdl[[i]] <- g(deltal[i],tk)
-        v[[i]]<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl[[i]]))
-        u[[i]]<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl[[i]]))^(1/4)
-        LA[[i]] <-  as.numeric(unlist(2 / (v[[i]]+v[[i]]^(-1)+u[[i]]^2)))
+        v[[i]]<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl[i]))
+        u[[i]]<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl[i]))^(1/4)
+        LA[[i]] <- as.numeric(2 / (v[[i]]+v[[i]]^(-1)+u[[i]]^2))
       }
-    }else{print("method not implemented yet")}
     }
+    #---------------------------------------------------------------------
+    # If the var.class = varExp
+    #---------------------------------------------------------------------
+    if(var.f == "varExp") {
+      if(attr(varcomp$modelStruct$varStruct, "formula")==~time){
+        gd <- g(delta,tk)
+        gdl <- g(deltal,tk)
+        v<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl))
+        u<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl))^(1/4)
+        LA <- list(2 / (v+v^(-1)+u^2), NA)
+      } else {if(attr(varcomp$modelStruct$varStruct, "formula")==~time | method){
+        gd <- g(delta,tk)
+        ldb2 <- length(deltal)
+        gdl<-list()
+        v <- list()
+        u <- list()
+        for(i in 1:ldb2){
+          gdl[[i]] <- g(deltal[i],tk)
+          v[[i]]<-sqrt((tGt+sig2_epsilon*gd)/(tGt+sig2_epsilon*gdl[[i]]))
+          u[[i]]<-sqrt(S2)/((tGt+sig2_epsilon*gd)*(tGt+sig2_epsilon*gdl[[i]]))^(1/4)
+          LA[[i]] <-  as.numeric(unlist(2 / (v[[i]]+v[[i]]^(-1)+u[[i]]^2)))
+        }
+      }else{print("method not implemented yet")}
+      }
+    }
+    #---------------------------------------------------------------------
+    # If var.class = NULL
+    #---------------------------------------------------------------------
+    if(var.f == "NULL"){
+      v<-sqrt((tGt+sig2_epsilon)/(tGt+sig2_epsilon))
+      u<-sqrt(S2)/((tGt+sig2_epsilon)*(tGt+sig2_epsilon))^(1/4)
+      LA <- list(2 / (v+v^(-1)+u^2), NA)
+    }
+    return(LA)
+  }else {
+    stop(print("Please, change the name of factor levels associated with
+               method argument. Example:  levels M1, M2, M3, ..., Mn,
+               with n=0, 1, 2, ..., N."), call. = FALSE)
   }
-  #---------------------------------------------------------------------
-  # If var.class = NULL
-  #---------------------------------------------------------------------
-  if(var.f == "NULL"){
-    v<-sqrt((tGt+sig2_epsilon)/(tGt+sig2_epsilon))
-    u<-sqrt(S2)/((tGt+sig2_epsilon)*(tGt+sig2_epsilon))^(1/4)
-    LA <- list(2 / (v+v^(-1)+u^2), NA)
-  }
-  return(LA)
 }
--- R/lccBuilder.R
+++ R/lccBuilder.R
@@ -34,47 +34,49 @@ lccBuilder <- function(G, diffbeta, tk, q_r, q_f, g, sig2_epsilon, delta,
   Tk_f <- sapply(0:q_f, function(x) tk^x)
   S2 <- try((as.matrix(Tk_f[,c(1:(length(diffbeta)))]) %*% (diffbeta))^2,
             silent = TRUE)
-  if(class(S2) == "try-error") {
-    stop(print("Please, change the name of factor levels associated with method argument. Example:  levels M1, M2, M3, ..., Mn, with n=0, 1, 2, ..., N."),
-         call. = FALSE)
-  }
-  tGt <- diag(Tk_r %*% G %*% t(Tk_r))
-  varcomp <- summary(model)
-  var.f <- class(varcomp$modelStruct$varStruct)[1]
-  rho <- list()
-  if(var.f == "varIdent") {
-    gd <- g(delta)
-    gdl <- g(deltal)
-    ldb2 <- length(gdl)
-    for(i in 1:ldb2){
-      rho[[i]] <- as.numeric(
-        tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl[i]) + S2))
-      )
-    }
-  }
-  if(var.f == "varExp") {
-    if(attr(varcomp$modelStruct$varStruct, "formula")==~time){
-      gd <- g(delta,tk)
-      gdl <- g(deltal,tk)
-      rho <-list(tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl) + S2)),NA)
-    } else {if(attr(varcomp$modelStruct$varStruct, "formula")==~time | method){
-      gd <- g(delta,tk)
-      ldb2 <- length(deltal)
-      gdl<-list()
+  if(all(class(S2) != "try-error")) {
+    tGt <- diag(Tk_r %*% G %*% t(Tk_r))
+    varcomp <- summary(model)
+    var.f <- class(varcomp$modelStruct$varStruct)[1]
+    rho <- list()
+    if(var.f == "varIdent") {
+      gd <- g(delta)
+      gdl <- g(deltal)
+      ldb2 <- length(gdl)
       for(i in 1:ldb2){
-        gdl[[i]] <- g(deltal[i],tk)
-        rho[[i]] <-
-          as.numeric(
-            unlist(
-              tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl[[i]]) + S2))
-            )
-          )
+        rho[[i]] <- as.numeric(
+          tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl[i]) + S2))
+        )
       }
-    }else{print("method not implemented yet")}
     }
+    if(var.f == "varExp") {
+      if(attr(varcomp$modelStruct$varStruct, "formula")==~time){
+        gd <- g(delta,tk)
+        gdl <- g(deltal,tk)
+        rho <-list(tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl) + S2)),NA)
+      } else {if(attr(varcomp$modelStruct$varStruct, "formula")==~time | method){
+        gd <- g(delta,tk)
+        ldb2 <- length(deltal)
+        gdl<-list()
+        for(i in 1:ldb2){
+          gdl[[i]] <- g(deltal[i],tk)
+          rho[[i]] <-
+            as.numeric(
+              unlist(
+                tGt / (tGt + 0.5 * (sig2_epsilon * (gd + gdl[[i]]) + S2))
+              )
+            )
+        }
+      }else{print("method not implemented yet")}
+      }
+    }
+    if(var.f == "NULL"){
+      rho<-list(tGt / (tGt + sig2_epsilon + 0.5*S2),NA)
+    }
+    return(rho)
+  }else{
+    stop(print("Please, change the name of factor levels associated with
+               method argument. Example:  levels M1, M2, M3, ..., Mn, with
+               n=0, 1, 2, ..., N."), call. = FALSE)
   }
-  if(var.f == "NULL"){
-    rho<-list(tGt / (tGt + sig2_epsilon + 0.5*S2),NA)
-  }
-  return(rho)
 }
