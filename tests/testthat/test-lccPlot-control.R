1ee0fefb3b71bddfc600fbc48a54bd2edd2091f3
Thiago de Paula Oliveira <toliveira@abacusbio.com>
2025-12-31 11:42:25 +0000

    new tests

--- tests/testthat/test-lccPlot-control.R
+++ tests/testthat/test-lccPlot-control.R
@@ -26,9 +26,40 @@ test_that("plotControl axis overrides are honoured", {
   b <- ggplot2::ggplot_build(p)
   params <- b$layout$panel_params[[1]]
 
+  first_non_null <- function(...) {
+    candidates <- list(...)
+    for (x in candidates) {
+      if (!is.null(x)) {
+        return(x)
+      }
+    }
+    NULL
+  }
+
+  safe_axis_component <- function(axis, attr_name, accessor = NULL) {
+    if (is.null(axis)) {
+      return(NULL)
+    }
+    val <- axis[[attr_name]]
+    if (!is.null(val)) {
+      return(val)
+    }
+    if (!is.null(accessor) && is.function(axis[[accessor]])) {
+      return(axis[[accessor]]())
+    }
+    NULL
+  }
+
   # Extract range/breaks in a way that is robust across ggplot2 versions
-  yrange <- params$y.range %||% params$y_range %||% params$y$range
-  ybreaks <- params$y.breaks %||% params$y$breaks %||% params$y$get_breaks()
+  yrange <- first_non_null(
+    params$y.range,
+    params$y_range,
+    safe_axis_component(params$y, "range", "range")
+  )
+  ybreaks <- first_non_null(
+    params$y.breaks,
+    safe_axis_component(params$y, "breaks", "get_breaks")
+  )
 
   expect_equal(yrange, c(0.7, 1))
   expect_equal(ybreaks, seq(0.7, 1, by = 0.1))
